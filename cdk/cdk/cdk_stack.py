af8b958122af308c20a8426c1ba82e9b294032de
koiker <koiker@amazon.com>
2024-11-18 10:05:37 -0500

    Added S3AG Location Role creation in CDK

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -152,18 +152,26 @@ class CdkStack(Stack):
             )
         )
 
+        s3ag_principal = iam.ServicePrincipal("access-grants.s3.amazonaws.com")
+        s3ag_principal.add_to_assume_role_policy(iam.PolicyDocument(
+            statements=[
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=[
+                        "sts:AssumeRole",
+                        "sts:SetSourceIdentity",
+                        "sts:SetContext"
+                    ],
+                    principals=[iam.ServicePrincipal("access-grants.s3.amazonaws.com")]
+                )
+            ]
+        ))
         s3ag_location_role = iam.Role(self, 'S3LocationRole',
-                                      assumed_by=iam.ServicePrincipal("access-grants.s3.amazonaws.com"),
-                                      trust_policy=iam.PolicyDocument(
-                                          statements=[
-                                              iam.PolicyStatement(
-                                                  effect=iam.Effect.ALLOW,
-                                                  actions=["sts:AssumeRole", "sts:SetSourceIdentity", "sts:SetContext"],
-                                                  principals=[iam.ServicePrincipal("access-grants.s3.amazonaws.com")],
-                                              )
-                                          ]
+                                      assumed_by=s3ag_principal,
+                                      role_name="S3AGLocationRole",
+                                      description="S3 Access Grant Location Role"
                                       )
-                                      )
+
         s3ag_location_role.add_to_policy(iam.PolicyStatement(
             sid="ObjectLevelReadPermissions",
             effect=iam.Effect.ALLOW,
@@ -226,7 +234,7 @@ class CdkStack(Stack):
                 "kms:Decrypt",
                 "kms:GenerateDataKey"
             ],
-            resources=['arn:aws:s3:::*'],
+            resources=['*'],
             conditions={
                 "StringEquals": {
                     "aws:ResourceAccount": self.account,
